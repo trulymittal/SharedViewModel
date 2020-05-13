5499a48aad43f2e11c48b125f78f57532ecff180
Truly Mittal <trulymittal@gmail.com>
2020-05-13 18:25:28 +0530

    Added menu item

--- app/src/main/java/com/example/sharedviewmodel/MainActivity.java
+++ app/src/main/java/com/example/sharedviewmodel/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.sharedviewmodel;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.action_add_item) {
+            Toast.makeText(this, "Add Item", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/example/sharedviewmodel/MasterFragment.java
+++ app/src/main/java/com/example/sharedviewmodel/MasterFragment.java
@@ -1,13 +1,12 @@
 package com.example.sharedviewmodel;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 
 /**
@@ -27,5 +26,6 @@ public class MasterFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_master, container, false);
+
     }
 }
